606f37b3b37fb9af1d9216ce55aa3a044c07f8ff
Ashley <ashjansen8@gmail.com>
2024-06-08 01:57:56 -0400

    trying

---
+++ typewriter.js
@@ -0,0 +1,20 @@
+const sentence = "hello there from lighthouse labs";
+
+const delay = function(sen) {
+  let string = sen
+  for (const char of string) {
+    //process.stdout.write(char);
+  console.log(char) 
+  
+  
+  
+  setTimeout(() => {
+  
+  },  3000); 
+  }
+
+  return setTimeout
+}
+
+
+
